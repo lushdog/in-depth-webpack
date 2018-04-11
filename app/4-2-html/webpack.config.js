e681220e2d8c810c4932dd81ff9656fadc99ea1e
impressioncr <impressioncr@hotmail.com>
2018-04-11 23:02:22 +0800

    Signed-off-by: impressioncr <impressioncr@hotmail.com>

--- app/4-2-html/webpack.config.js
+++ app/4-2-html/webpack.config.js
@@ -9,14 +9,10 @@ module.exports = {
     app: './src/app.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
-    filename: '[name].bundle.js'
-  },
-  resolve: {
-    alias: {
-      jquery$: path.resolve(__dirname, 'src/libs/jquery.min.js')
-    }
+    path: path.resolve(__dirname, './dist'),
+    publicPath: './dist/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js'
   },
   module: {
     rules: [
@@ -53,72 +49,10 @@ module.exports = {
               loader: 'css-loader'
             },
             {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: [
-                  require('postcss-sprites')({
-                    spritePath: 'dist/assets/imgs/sprites'
-                  })
-                ]
-              }
-            },
-            {
               loader: 'less-loader'
             }
           ]
         })
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/,
-        use: [
-          // {
-          //   loader: 'file-loader',
-          //   options: {
-          //     useRelativePath: true
-          //   }
-          // }
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name].min.[ext]',
-              limit: 1000,
-              useRelativePath: true
-            }
-          },
-          {
-            loader: 'img-loader',
-            options: {
-              pngquant: {
-                quality: 80
-              }
-            }
-          },
-        ]
-      },
-      {
-        test: /\.(eot|woff2?|ttf|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: '[name].min.[ext]',
-              limit: 5000,
-              useRelativePath: true
-            }
-          }
-        ]
-      },
-      {
-        test: path.resolve(__dirname, 'src/app.js'),
-        use: [
-          {
-            loader: 'imports-loader',
-            options: {
-              $: 'jquery'
-            }
-          }
-        ]
       }
     ]
   },
@@ -134,9 +68,7 @@ module.exports = {
         path.join(__dirname, './src/*.js')
       ])
     }),
-    new webpack.ProvidePlugin({
-      $: 'jquery'
-    }),
+
     new webpack.optimize.UglifyJsPlugin()
   ]
 }
